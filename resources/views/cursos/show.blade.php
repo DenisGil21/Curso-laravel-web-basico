4cd894c6570b219dd8518fa3cac372fb62f677bb
DenisGil21 <denisgilsil.21@gmail.com>
2022-01-19 14:16:44 -0600

    Eliminar registros

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -5,8 +5,14 @@
 
 @section('content')
     <h1>Curso {{ $curso->name }}</h1>
-    <a href="{{route('cursos.index')}}">Volver a curso</a><br>
-    <a href="{{route('cursos.edit',$curso)}}">Editar curso</a>
+    <a href="{{ route('cursos.index') }}">Volver a curso</a><br>
+    <a href="{{ route('cursos.edit', $curso) }}">Editar curso</a>
     <p><strong>Categoria: </strong>{{ $curso->categoria }}</p>
     <p><strong>Descripcion: </strong> {{ $curso->description }}</p>
+
+    <form action="{{ route('cursos.destroy', $curso) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Eliminar</button>
+    </form>
 @endsection
